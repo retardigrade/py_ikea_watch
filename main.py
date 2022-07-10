4a3194960f701962058e042c85e313c322e1e7c6
Petr Martynenko <pmartynenko@ozon.ru>
2022-07-10 12:47:34 +0300

    added new watch phrase

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from aiogram import Bot, Dispatcher, executor, types
 
 API_TOKEN = os.environ['API_TOKEN']
 URL = 'https://www.ikea.com/ru/ru/'
-WATCH_PHRASE_1 = 'Прием новых заявок запустится немного позже'
-WATCH_PHRASE_2 = 'Мы вернём вас на IKEA.ru, как только это будет возможно'
+WATCH_PHRASE_1 = 'новых заявок запустится немного позже'
+WATCH_PHRASE_2 = 'вернём вас на IKEA.ru, как только это будет возможно'
+WATCH_PHRASE_3 = 'овершить покупку сейчас не получится'
 
 
 logging.basicConfig(level=logging.INFO)
@@ -31,7 +32,7 @@ async def watch(message: types.Message):
         if resp.status_code != 200:
             await message.answer(f"HTTP code {resp.status_code}: couldn't fetch page")
             break
-        if WATCH_PHRASE_1 not in resp.text and WATCH_PHRASE_2 not in resp.text:
+        if WATCH_PHRASE_1 not in resp.text and WATCH_PHRASE_2 not in resp.text and WATCH_PHRASE_3 not in resp.text:
             await message.answer('Form available. Visit https://www.ikea.com/ru/ru/')
             break
         time.sleep(15)
