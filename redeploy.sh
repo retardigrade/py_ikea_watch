12a6ef1455fbe9253c94bcf7878f09144003fa23
Petr Martynenko <pmartynenko@ozon.ru>
2022-07-10 13:41:00 +0300

    update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 token=$1
+date=$(date '+%Y-%m-%d--%H:%M:%S')
+
 cd ./py_ikea_watch || exit
 git pull origin main
 
 docker stop ikea_bot
 docker image rm ikea_bot:latest -f
 docker build . --tag ikea_bot
-docker run --name ikea_bot -e="$token" --restart unless-stopped ikea_bot:latest
+docker run --name ikea_bot_"$date" -e="$token" --restart unless-stopped ikea_bot:latest
